ffb75a33724aeee7c306443dd555209fc76609f2
Keenah <kvancamp@mail.com>
2022-08-16 13:07:50 -0500

    Create Nav.js

---
+++ src/Nav.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  const navigate = useNavigate();
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <div className="nav__contents">
+        <img
+          onClick={() => navigate("/")}
+          className="nav__logo"
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt=""
+        />
+
+        <img
+          onClick={() => navigate("/profile")}
+          className="nav__avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
