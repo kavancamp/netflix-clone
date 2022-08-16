45acdfbbff713dd8e8107b8e73aa677c5034d523
Keenah <kvancamp@mail.com>
2022-08-16 13:07:29 -0500

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCdNYg9Jfmo1qg9cqsAF1T2gHV651SBcEQ",
+  authDomain: "netflix-clone-9bab9.firebaseapp.com",
+  projectId: "netflix-clone-9bab9",
+  storageBucket: "netflix-clone-9bab9.appspot.com",
+  messagingSenderId: "622111214692",
+  appId: "1:622111214692:web:9488d2d56152fa75cb2371",
+  measurementId: "G-ST040X5CX6"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const  auth = firebase.auth();
+
+export { auth } ;
+export default db;
