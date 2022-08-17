cd3cab86f710e2021fd585361148b425af1bac1b
Keenah <kvancamp@mail.com>
2022-08-17 12:42:26 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import  { HomeScreen } from "./screens/HomeScreen";
+import  HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
 import ProfileScreen from "./screens/ProfileScreen";
