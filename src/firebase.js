cfde718ab7680d4d1bbb1110e9cdf0a26a9ac825
Keenah <kvancamp@mail.com>
2023-04-27 09:57:12 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,21 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import firebase_api_key from ".env";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCdNYg9Jfmo1qg9cqsAF1T2gHV651SBcEQ",
+  apiKey: firebase_api_key,
   authDomain: "netflix-clone-9bab9.firebaseapp.com",
   projectId: "netflix-clone-9bab9",
   storageBucket: "netflix-clone-9bab9.appspot.com",
   messagingSenderId: "622111214692",
   appId: "1:622111214692:web:9488d2d56152fa75cb2371",
-  measurementId: "G-ST040X5CX6"
+  measurementId: "G-ST040X5CX6",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
-const  auth = firebase.auth();
+const auth = firebase.auth();
 
-export { auth } ;
+export { auth };
 export default db;
